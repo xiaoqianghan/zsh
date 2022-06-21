4670ff081537f05c0fb4bdec806c561aefc72b5b
xiaoqiang han <97718268+xiaoqianghan@users.noreply.github.com>
2022-06-21 08:25:45 +0800

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,22 +1,18 @@
 alias ll="exa -alh"
 alias la="exa -lh"
+
 catpb(){
   cat "$1" | pbcopy
 }
 alias t="tmux"
 
-alias cdtmp="cd /var/folders/j2/4ftvc7pj6b59rx9fxbtw3v100000gp/T/"
-alias dk="docker"
 alias cls="clear"
 
-alias pushneo="git push https://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@github.com/xiaoqianghan/neovim.git"
 alias lg="lazygit"
 alias glog="git log -p -- filename"
-alias neo="cd /Users/xiaoqianghan/.config/nvim"
 
 alias cdinfra="cd /Users/xiaoqianghan/kode/infrastructure"
 alias activeinfra="conda activate infra"
-
 
 # Java
 alias java-algs4="java -cp .:/Users/xiaoqianghan/Desktop/coursera/lib/algs4.jar"
@@ -25,7 +21,6 @@ alias javac-algs4="javac -cp .:/Users/xiaoqianghan/Desktop/coursera/lib/algs4.ja
 # Python
 alias python="python3"
 alias pip="pip3"
-alias py="python3"
 
 # Elixir
 alias mxf="mix format"
